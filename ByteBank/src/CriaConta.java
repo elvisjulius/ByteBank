27e94ba7e615d6ad10c8cb5b43eb1b9e4b12540b
Elvis Julius <elvisjulius@hotmail.com>
2021-02-23 21:42:02 -0300

    Criar Conta 2

--- ByteBank/src/CriaConta.java
+++ ByteBank/src/CriaConta.java
@@ -2,9 +2,15 @@
 public class CriaConta {
 
 	public static void main(String[] args) {
-		
+
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
 		System.out.println(primeiraConta.saldo);
+		primeiraConta.saldo += 100;
+		System.out.println(primeiraConta.saldo);
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		System.out.println("Na Primeira conta tem " + primeiraConta.saldo + " reais");
+		System.out.println("Na Segunda conta tem " + segundaConta.saldo + " reais");
 	}
 }
